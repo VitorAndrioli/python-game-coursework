106e668c18c5f4498c9dda4a5baf3972c7d42c44
VitorAndrioli <vtr_andrioli@hotmail.com>
2013-06-01 02:46:51 -0300

    walking

--- gameSprite.py
+++ gameSprite.py
@@ -5,14 +5,19 @@ class GameSprite():
         self.bricks = bricks;
         self.pos = pos;
         self.counter = 0;
+        self.animationSpeed = 10;
+        self.animationStep = 0;
         
     def update(self):
         self.rect = self.bricks[self.counter].image.get_rect();
         self.rect = self.rect.move(self.pos[0], self.pos[1]);
-        self.counter+=1;
-        if (self.counter >= len(self.bricks)):
-            self.counter = 0;
-            
+        self.animationStep+=1;
+        if (self.animationStep == self.animationSpeed):
+            self.counter+=1;
+            if (self.counter == (len(self.bricks)-1)):
+                self.counter = 0;
+            self.animationStep = 0;
+    
     def render(self, surface):
         surface.blit(self.bricks[self.counter].getImage(), self.rect);
         
--- main.py
+++ main.py
@@ -10,9 +10,13 @@ clock = pygame.time.Clock();
 tileFranky = Tile("img/sprite_t.png", 240, 190);
 
 bricksStand = [Brick(0, 0, tileFranky), Brick(1, 1, tileFranky), Brick(2, 2, tileFranky), Brick(3, 3, tileFranky)];
+bricksWalk = [Brick(4, 4, tileFranky), Brick(5, 5, tileFranky), Brick(6, 6, tileFranky), Brick(7, 7, tileFranky), Brick(8, 8, tileFranky), Brick(9, 9, tileFranky)];
 
 franky = GameSprite(bricksStand, (100, 100));
 
+moveRight = False;
+moveLeft = False;
+stand = True;
 while(True):
     
     surface.fill((0, 0, 0));
@@ -20,8 +24,31 @@ while(True):
     franky.render(surface);
     
     pygame.display.update();
-    clock.tick(5);
+    clock.tick(30);
     
     for e in pygame.event.get():
-        if e.type == pygame.locals.QUIT:
+        if (e.type == pygame.locals.KEYDOWN):
+            if (e.key == pygame.locals.K_RIGHT):
+                moveRight = True;
+                stand = False;
+            elif (e.key == pygame.locals.K_LEFT):
+                moveLeft = True;
+                stand = False; 
+        elif (e.type == pygame.locals.KEYUP):
+            if (e.key == pygame.locals.K_RIGHT):
+                moveRight = False;
+                stand = True;
+            elif (e.key == pygame.locals.K_LEFT):
+                moveLeft = False;
+                stand = True;
+        elif (e.type == pygame.locals.QUIT):
             exit();
+            
+    
+    if (stand):
+        franky.bricks = bricksStand;
+    if (moveRight):
+        #franky.setPosition((franky.pos[0]+5, franky.pos[1]));
+        franky.bricks = bricksWalk;
+    if (moveLeft):
+        franky.setPosition((franky.pos[0]-10, franky.pos[1]));
