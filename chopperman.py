1f6a42770e9a9c4623e41fdcee4d84c6ffaa1d96
VitorAndrioli <vtr_andrioli@hotmail.com>
2013-06-09 15:48:57 -0300

    chopperman class

---
+++ chopperman.py
@@ -0,0 +1,93 @@
+from brick import Brick;
+from tile import Tile;
+import Box2D as b2;
+import pygame;
+
+class Chopperman():
+    def __init__(self, pos, surface, world, PPM):
+        
+        self.width = 50;
+        self.height = 45;
+        
+        self.counter = 0;
+        self.animationStep = 0;
+        self.animationSpeed = 3;
+        
+        self.tile = Tile("img/chopperman.png", self.width, self.height);
+        self.bricks = [Brick(0, 0, self.tile), Brick(1, 1, self.tile), Brick(2, 2, self.tile), Brick(3, 3, self.tile)];
+        
+        self.PPM = PPM;
+        self.surface = surface;
+        self.world = world;
+        
+        self.up = True;
+        
+        self.createPhysicalBody(pos);
+        
+    def createPhysicalBody(self, pos):
+        bodyDef = b2.b2BodyDef();
+        bodyDef.position = pos;
+        bodyDef.angle = 0;
+        bodyDef.type = b2.b2_dynamicBody;
+        self.body = self.world.CreateBody( bodyDef );
+        
+        bodyFixture = b2.b2FixtureDef();
+        bodyFixture.density = 0;
+        bodyFixture.friction = 0.0;
+        bodyFixture.restitution = 0.0;
+        bodyFixture.shape = b2.b2PolygonShape( box = (self.width/2, self.height/2) );
+        
+        self.up = True;
+        self.high = 0;
+        self.highValue = 1;
+        self.body.CreateFixture(bodyFixture);
+        
+    def update(self):
+        self.high +=1;
+        if (self.high == 15):
+            self.high = 0;
+            if (self.up):
+                self.up = False;
+                self.highValue = -1;
+            else:
+                self.up = True;
+                self.highValue = 1;
+           
+        self.body.position = (self.body.position[0] + 5, self.body.position[1] + self.highValue);
+        self.body.ApplyForce((0, -10), self.body.position, True);
+        
+        
+        self.animationStep += 1;
+        if (self.animationStep == self.animationSpeed):
+            self.counter += 1;
+            if (self.counter == len(self.bricks)):
+                self.counter = 0;
+            self.animationStep = 0;
+        self.render();
+    
+    def render(self):
+        shape = self.body.fixtures[0].shape;
+        pixelVertices = [];
+        for vertice in shape.vertices:
+            v = self.body.transform * vertice * self.PPM;
+            pixelVertices.append(v);
+            
+        #pygame.draw.polygon(self.surface, (255, 0, 0), pixelVertices);
+        self.surface.blit(self.bricks[self.counter].getImage(), (self.body.position[0] - self.width/2, self.body.position[1] - self.height/2));
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+            
+        
+        
--- plant.py
+++ plant.py
@@ -1,18 +1,36 @@
 from brick import Brick;
 from tile import Tile;
+import Box2D as b2;
+import pygame
 
 class Plant():
-    def __init__( self, pos, surface ):
+    def __init__(self, pos, surface, world, PPM):
+        #physical variables
         self.pos = pos;
-        self.tile = Tile("img/killer_plant.png", 96, 85);
+        self.width = 96;
+        self.height = 75;
+        
+        #animation variables
         self.counter = 0;
         self.animationSpeed = 9;
         self.animationStep = 0;
-        self.surface = surface;
+        
+        self.tile = Tile("img/killer_plant.png", self.width, self.height);
         self.bricksStand = [Brick(0, 0, self.tile), Brick(0, 1, self.tile)];
         self.bricksAttack = [Brick(0, 2, self.tile), Brick(0, 3, self.tile), Brick(0, 3, self.tile)]; 
+        
         self.bricks = self.bricksStand;
+        
+        #drawing variables
+        self.PPM = PPM;
+        self.surface = surface;
+        self.world = world;
+        
+        #checking variables
         self.attacking = False;
+        self.colision = False;
+        
+        self.createPhysicalBody();
         
     def attack(self):
         self.counter = 0;
@@ -27,7 +45,8 @@ class Plant():
         self.animationSpeed = self.animationSpeed * 3;
         self.attacking = False;
         
-    def update( self ):
+    def update(self):
+        self.checkColision();
         self.animationStep += 1;
         if (self.animationStep == self.animationSpeed):
             self.counter += 1;
@@ -38,8 +57,32 @@ class Plant():
             self.animationStep = 0;
         self.render();
         
-    def render( self ):
+    def render(self):
+        shape = self.body.fixtures[0].shape;
+        pixelVertices = [];
+        for vertice in shape.vertices:
+            v = self.body.transform * vertice * self.PPM;
+            pixelVertices.append(v);
+            
+        #pygame.draw.polygon(self.surface, (0, 255, 0), pixelVertices);
         self.surface.blit(self.bricks[self.counter].getImage(), self.pos);
         
+   
+    def checkColision(self):
+        if (self.colision):
+            self.attack();
+            self.colision = False;
+            
+    def createPhysicalBody(self):
+        bodyDef = b2.b2BodyDef();
+        bodyDef.position = (self.pos[0]/self.PPM + self.width/2, self.pos[1]/self.PPM + self.height/2);
+        bodyDef.angle = 0;
+        bodyDef.type = b2.b2_staticBody;
+        self.body = self.world.CreateBody(bodyDef);
+    
+        bodyFixture = b2.b2FixtureDef();
+        bodyFixture.shape = b2.b2PolygonShape( box = (self.width/2, self.height/2) );
+        
+        self.body.CreateFixture(bodyFixture);
         
         
